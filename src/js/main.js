c63709fab0992c6d901e2db0a5fe70730ee33e44
stas <st.nagoryansky@gmail.com>
2023-12-12 18:32:43 +0200

    redaction

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+import '../scss/styles.scss'
+import * as bootstrap from 'bootstrap'
+import Alert from 'bootstrap/js/dist/alert'
+
+// or, specify which plugins you need:
+import { Tooltip, Toast, Popover } from 'bootstrap'
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+
+    alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+        appendAlert('Nice, you triggered this alert message!', 'success')
+    })
+}
